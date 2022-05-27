6e0e83b9aec36c44f1a4e818dce76fd6287695eb
ersushantsood@gmail.com <ersushantsood@gmail.com>
2022-05-26 22:10:51 -0700

    added comments

--- Trie-implementation/java/src/main/java/Solution.java
+++ Trie-implementation/java/src/main/java/Solution.java
@@ -6,18 +6,12 @@ public class Solution {
     public static void main(String[] args){
 
         Trie<Boolean> trie =  TrieBuilder.<Boolean>newBuilder()
-                .put("new", true)
-                .put("newton", true)
-                .put("nuke", true)
-                .put("numeric", true)
-                .put("number", true)
-                .put("numb", true)
-                .put("java", true)
-                .put("netizen", true)
-                .put("germany", true)
-                .put("potato", true)
-                .put("rambo", true)
-                .put("umbrella", true)
+                .put("abc", true)
+                .put("abcd", true)
+                .put("abcde", true)
+                .put("abcdef", true)
+                .put("abcdefg", true)
+                .put("abcdefgh", true)
                 .build();
 
         System.out.println("Check if nuke exists: "+trie.getOrNull("nuke"));
